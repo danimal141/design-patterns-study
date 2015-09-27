f4bdec5f94966f56c9528315920b7f60973acbb4
danimal141 <hideaki.ishii1204@gmail.com>
2015-09-27 15:55:39 +0900

    Add Factory pattern

---
+++ Factory/src/Rectangle.java
@@ -0,0 +1,9 @@
+/**
+ * Created by hideaki on 9/27/15.
+ */
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
---
+++ Factory/src/Shape.java
@@ -0,0 +1,6 @@
+/**
+ * Created by hideaki on 9/27/15.
+ */
+public interface Shape {
+    void draw();
+}
