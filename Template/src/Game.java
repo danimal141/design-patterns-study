f670cf733e6f15a6c3e286af90e5e83c06c95071
danimal141 <hideaki.ishii1204@gmail.com>
2015-09-27 15:11:16 +0900

    Add Template pattern

---
+++ Template/src/Game.java
@@ -0,0 +1,19 @@
+/**
+ * Created by hideaki on 9/27/15.
+ */
+public abstract class Game {
+    abstract void initialize();
+    abstract void startPlay();
+    abstract void endPlay();
+
+    public final void play() {
+        //initialize the game
+        this.initialize();
+
+        //start game
+        this.startPlay();
+
+        //end game
+        this.endPlay();
+    }
+}
---
+++ Template/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by hideaki on 9/27/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Cricket();
+        game.play();
+
+        System.out.println();
+
+        game = new Football();
+        game.play();
+    }
+}
