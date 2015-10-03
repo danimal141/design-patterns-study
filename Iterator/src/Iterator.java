c296734fb1594a32e0d313238b4d44cef997991e
danimal141 <hideaki.ishii1204@gmail.com>
2015-10-03 12:11:23 +0900

    Update Iterator pattern

--- Iterator/src/Iterator.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by hideaki on 9/27/15.
- */
-public interface Iterator {
-    public boolean hasNext();
-    public Object next();
-}
--- Iterator/src/Main.java
+++ Iterator/src/Main.java
@@ -1,14 +1,35 @@
 /**
  * Created by hideaki on 9/27/15.
  */
+import java.util.Iterator;
+
 public class Main {
 
     public static void main(String[] args) {
-        NameRepository namesRepository = new NameRepository();
+        Item i1 = new Item("spaghetti", 7.50f);
+        Item i2 = new Item("hamburger", 6.00f);
+        Item i3 = new Item("chicken sandwich", 6.50f);
 
-        for(Iterator iter = namesRepository.getIterator(); iter.hasNext();){
-            String name = (String)iter.next();
-            System.out.println("Name : " + name);
+        Menu menu = new Menu();
+        menu.addItem(i1);
+        menu.addItem(i2);
+        menu.addItem(i3);
+
+        System.out.println("Displaying Menu:");
+        Iterator<Item> iterator = menu.iterator();
+        while (iterator.hasNext()) {
+            Item item = iterator.next();
+            System.out.println(item);
+        }
+
+        System.out.println("\nRemoving last item returned");
+        iterator.remove();
+
+        System.out.println("\nDisplaying Menu:");
+        iterator = menu.iterator();
+        while (iterator.hasNext()) {
+            Item item = iterator.next();
+            System.out.println(item);
         }
     }
 
