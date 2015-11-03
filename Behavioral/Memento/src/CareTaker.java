1ddd7262dd971b89489d9df83dad239d6242b52b
danimal141 <hideaki.ishii1204@gmail.com>
2015-11-03 14:52:57 +0900

    Add Memento pattern

---
+++ Behavioral/Memento/src/CareTaker.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hideaki on 11/3/15.
+ */
+public class CareTaker {
+
+    private List<Memento> mementoList = new ArrayList<Memento>();
+
+    public void add(Memento state){
+        mementoList.add(state);
+    }
+
+    public Memento get(int index){
+        return mementoList.get(index);
+    }
+
+}
---
+++ Behavioral/Memento/src/Memento.java
@@ -0,0 +1,16 @@
+/**
+ * Created by hideaki on 11/3/15.
+ */
+public class Memento {
+
+    private String state;
+
+    public Memento(String state){
+        this.state = state;
+    }
+
+    public String getState(){
+        return state;
+    }
+
+}
