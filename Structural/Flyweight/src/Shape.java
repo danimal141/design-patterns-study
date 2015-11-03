7df3396da5e1c9acf50a476503b93b58a2df0cad
danimal141 <hideaki.ishii1204@gmail.com>
2015-11-03 21:38:24 +0900

    Add Flyweight pattern

---
+++ Structural/Flyweight/src/Shape.java
@@ -0,0 +1,6 @@
+/**
+ * Created by hideaki on 11/3/15.
+ */
+public interface Shape {
+    void draw();
+}
---
+++ Structural/Flyweight/src/ShapeFactory.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+/**
+ * Created by hideaki on 11/3/15.
+ */
+public class ShapeFactory {
+
+    private static final HashMap<String, Shape> circleMap = new HashMap();
+
+    public static Shape getCircle(String color) {
+        Circle circle = (Circle) circleMap.get(color);
+
+        if (circle == null) {
+            circle = new Circle(color);
+            circleMap.put(color, circle);
+            System.out.println("Creating circle of color : " + color);
+        }
+        return circle;
+    }
+
+}
